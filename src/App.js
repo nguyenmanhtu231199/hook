557ac2a2ec0e6b3a9abebf0a05678d7b562885f6
nguenmanhtu <nguyenmanhtu231199@gmail.com>
2021-07-09 17:10:08 +0700

    post list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import PostList from './components/PostList';
 function App() {
   const [todoList, setTodoList]= useState([
     { id: 1, title :'Anh yeu em '},
     { id : 2, title :' Anh nho em '},
     { id: 3, title : ' Anh can em '},
   ]);
+  const [postList,setPostList]= useState([]);
+  useEffect(()=>{
+async function fetchPostList (){
+  try {
+  const requestUrl ='http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+  const response = await fetch(requestUrl);
+  const responseJson = await response.json();
+  console.log({responseJson});
+  const {data}=responseJson;
+  setPostList(data);
+    
+  } catch (error) {
+    console.log('Failed ', error.message);
+  }
+}
+fetchPostList();
+  },[]);
 function handleTodoClick(todo){
 const index = todoList.findIndex(x => x.id == todo.id);
 if(index <0) return;
@@ -30,8 +48,10 @@ function handleTodoFormSubmit(formValues){
   return (
     <div className="app">
       <h1>anhyeuem</h1>
+      <ColorBox/>
       <TodoForm  onsubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
+      <PostList posts={postList} />
     </div>
   );
 }
